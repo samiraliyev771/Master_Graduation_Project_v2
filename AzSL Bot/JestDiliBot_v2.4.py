8b23cc1b3b224a4f4f3f6b6cecf228c24cfba085
samiraliyev771 <61183978+samiraliyev771@users.noreply.github.com>
2021-08-02 20:17:07 +0400

    Update JestDiliBot_v2.4.py

--- AzSL Bot/JestDiliBot_v2.4.py
+++ AzSL Bot/JestDiliBot_v2.4.py
@@ -276,7 +276,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1780780782:AAGSGKBe12aNd9Yh82VpkEQvD1vrMMf1IvM", use_context=True)
+    updater = Updater("your_token", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
